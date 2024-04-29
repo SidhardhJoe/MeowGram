cc98789d69f0a3c2b04810e9b37bd2bad8d6fc47
Sidhardh Joe <sidhardhjoe@gmail.com>
2024-04-29 17:53:40 +0530

    commit

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <SafeAreaView>
+      <ScrollView style={styles.main}>
+        <StatusBar backgroundColor="white"
+          barStyle="light-content"></StatusBar>
+        <StatusBar />
 
-      </SafeAreaView>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      </ScrollView>
     </View>
   );
 }
@@ -16,8 +18,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   }
+
 });
