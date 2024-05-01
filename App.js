3d01b1314713a7784d494f33fc558051e65537eb
Sidhardh Joe <sidhardhjoe@gmail.com>
2024-05-01 09:25:53 +0530

    Main Page

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 
-import { StyleSheet, View, ScrollView, Image } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginPage from './src/screen/LoginPage';
@@ -9,12 +9,14 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="LoginPage" component={LoginPage} />
-        <Stack.Screen name="MainPage" component={MainPage} />
-      </Stack.Navigator>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="LoginPage" component={LoginPage} />
+          <Stack.Screen name="MainPage" component={MainPage} />
+        </Stack.Navigator>
 
-    </NavigationContainer>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 }
--- src/screen/MainPage.js
+++ src/screen/MainPage.js
@@ -1,14 +1,67 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image, PermissionsAndroid } from 'react-native'
 import React from 'react'
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
+const requestCameraPermission = async () => {
+    try {
+        const granted = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.CAMERA,
+            {
+                title: 'Camera',
+                buttonNegative: 'Cancel',
+                buttonPositive: 'OK',
+            },
+        );
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+            console.log('You can use the camera');
+        } else {
+            console.log('Camera permission denied');
+        }
+    } catch (err) {
+        console.warn(err);
+    }
+};
 
 const MainPage = () => {
     return (
         <View>
-            <Text>MainPage</Text>
+            <View style={styles.main}>
+                <TouchableOpacity onPress={requestCameraPermission}>
+                    <Image source={require("../../assets/Images/camera.png")} style={styles.camera} />
+                </TouchableOpacity>
+                <Image source={require("../../assets/Images/InstagramText.png")} style={styles.iglogo} />
+                <Image source={require("../../assets/Images/send.png")} style={styles.chat} />
+            </View>
+
         </View>
     )
 }
 
 export default MainPage
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    main: {
+        display: "flex",
+        flexDirection: "row"
+    },
+    camera: {
+        width: 40,
+        height: 40,
+        marginTop: "100%",
+        padding: 20,
+        marginLeft: "2%",
+    },
+    iglogo: {
+        height: 50,
+        width: 150,
+        marginTop: "13%",
+        marginLeft: "22%",
+        objectFit: "contain"
+    },
+    chat: {
+        height: 40,
+        width: 40,
+        marginTop: "12%",
+        marginLeft: "17%"
+    }
+})
