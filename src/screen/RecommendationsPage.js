1e2d67ee43227546fbaca55b2c3846197d1532ab
Sidhardh Joe <sidhardhjoe@gmail.com>
2024-05-01 18:20:54 +0530

    Final Commit Day  2

--- src/screen/RecommendationsPage.js
+++ src/screen/RecommendationsPage.js
@@ -1,12 +1,18 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, Image } from 'react-native'
 import React from 'react'
+import { StyleSheet } from 'react-native-web'
 
 const RecommendationsPage = () => {
     return (
-        <View style={{ backgroundColor: "red", height: "100%" }}>
-            <Text>RecommendationsPage</Text>
-        </View>
+        <ScrollView style={{ height: "100%" }}>
+            <Image source={require("../../assets/Images/user.png")} style={styles.profilepic} />
+        </ScrollView>
     )
 }
 
 export default RecommendationsPage
+const styles = StyleSheet.create({
+    profilepic: {
+        height: "10"
+    }
+})
