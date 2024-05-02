71ed130a7f5e1f4417c1e4e298be5a0c8ec4df53
Sidhardh Joe <sidhardhjoe@gmail.com>
2024-05-02 15:15:03 +0530

    Changes

--- src/components/EditProfileButton.js
+++ src/components/EditProfileButton.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
-import EditProfileScreen from '../screen/EditProfileScreen';
 import { StyleSheet } from 'react-native';
 
 
